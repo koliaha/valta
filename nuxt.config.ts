76528b9aa1b771e21352ef644021498ecf8140dd
Nikolaychik <nikolay@gomobile.ru>
2023-01-29 19:56:01 +0300

    commit init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  router: {
+    base: "/nuxt-github-pages/", // base: '/<repository>/'
+  },
+  build: {
+    publicPath: "./",
+  },
+});
